ef53a09111fec529a29ba3a1c795b5ff3fb280a5
Andrew Look <andrew.m.look@gmail.com>
2018-11-26 21:29:24 -0500

    train scripts

---
+++ train_hands.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+IMAGES=$(pwd)/images
+
+STYLE_IMG=$IMAGES/pastel_hands.jpg
+TEST_IMG=$IMAGES/real_sitting.jpg
+
+mkdir -p /output/model
+mkdir -p /output/test
+
+python style.py --style $STYLE_IMG \
+  --checkpoint-dir /output/model \
+  --test $TEST_IMG \
+  --test-dir /output/test \
+  --content-weight 1.5e1 \
+  --checkpoint-iterations 1000 \
+  --batch-size 20
